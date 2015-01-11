abb2694d76764f8191d9a4c683d1b0a14a277d6f
Barbara <barbasien@wp.pl>
2015-01-11 16:44:54 +0100

    Files extracted directly to data folder. Otherwise I couldn't list the files. Propably because of "-" in the folder's names (e.g. netherlands-places-shape)

--- R/1_ZipFileDownloader.R
+++ R/1_ZipFileDownloader.R
@@ -6,19 +6,23 @@
 # Function to download and extract specified zip files to the data folder of R projects
 # may need adjustment
 
+
 ZipFileDownloader <- function(url, filedestination, filename) {
   
-  deposit <- paste(filedestination, filename)
-  file <- strsplit(filename, ".zip")
-  filelocation <- paste("data/")
+  deposit <- paste(filedestination, filename) # Gives the directory of the zip file
+  file <- strsplit(filename, ".zip") # Gives the name of the file (without extension)
+  filelocation <- paste("data/", file)
   download.file(url, deposit)
   
-  # Extract files
-  unzip(deposit, files = NULL, list = FALSE, overwrite = TRUE,junkpaths = FALSE, 
-        exdir = filelocation, unzip = "internal", setTimes = FALSE)
+  # Extract files:
+  unzip(deposit, files = NULL, list = FALSE, overwrite = TRUE, junkpaths = FALSE, 
+        exdir = "data", unzip = "internal", setTimes = FALSE)
 
-  # Find zip and remove it
+  # Find zip and remove it:
   zip <- list.files("data/", pattern=glob2rx("*.zip"), full.names = TRUE)
   file.remove(zip)
 
 }
+
+
+
